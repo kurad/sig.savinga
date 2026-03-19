569eb3db410124dd99967f3bb7578cbc2e65e1d4
kurad <bendusenge@gmail.com>
2026-03-19 20:24:41 +0200

    CORS Fixture

--- config/cors.php
+++ config/cors.php
@@ -20,10 +20,10 @@ return [
     'allowed_methods' => ['*'],
 
     'allowed_origins' => [
-        'http://localhost:8080', 
-        'http://127.0.0.1:8080',
-        'http://localhost:8081', 
-        'http://127.0.0.1:8081',
+        // 'http://localhost:8080', 
+        // 'http://127.0.0.1:8080',
+        // 'http://localhost:8081', 
+        // 'http://127.0.0.1:8081',
         'https://savinga.rw'
         ],
 
@@ -35,6 +35,6 @@ return [
 
     'max_age' => 0,
 
-    'supports_credentials' => false,
+    'supports_credentials' => true,
 
 ];
